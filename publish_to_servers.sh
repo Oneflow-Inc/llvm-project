d655d810186951e8a89b42df30868273d3318ac2
daquexian <daquexian566@gmail.com>
2023-03-22 16:28:02 +0800

    update publish_to_servers.sh
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- publish_to_servers.sh
+++ publish_to_servers.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
-# Usage: ./publish_to_servers.sh clangd-14.AppImage sudo_passwd
+# Usage: ./publish_to_servers.sh clangd-14.AppImage
 
 set -euxo pipefail
 
 function upload {
-  scp $1 $3:/tmp
-  bn=$(basename $1)
-  # `sudo` is intercepted by /opt/rh/devtoolset-7/root/usr/bin/sudo in some machines
-  ssh $3 "echo $2 | /usr/bin/sudo -S cp /tmp/$bn /usr/local/bin"
-  ssh $3 "rm /tmp/$bn"
+  scp $1 $2:$3
+  # scp $1 $3:/tmp
+  # bn=$(basename $1)
+  # # `sudo` is intercepted by /opt/rh/devtoolset-7/root/usr/bin/sudo in some machines
+  # ssh $3 "echo $2 | /usr/bin/sudo -S cp /tmp/$bn /usr/local/bin"
+  # ssh $3 "rm /tmp/$bn"
 }
 
 for x in `seq 11 16`
 do
-  upload $1 $2 $x-suzhu
+  upload $1 $x-suzhu /dataset/
 done
 
 for x in `seq 21 23`
 do
-  upload $1 $2 $x-suzhu
+  upload $1 $x-suzhu /dataset/
 done
 
 for x in `seq 25 26`
 do
-  upload $1 $2 $x-suzhu
+  upload $1 $x-suzhu /data/dataset
 done
